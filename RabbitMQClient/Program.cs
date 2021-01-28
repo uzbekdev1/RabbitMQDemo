d10fef0099dd8876b8f7bcd793ba68f82430dea2
Elyor <elyor.dev@gmail.com>
2021-01-28 23:00:19 +0500

    rabbitmq demo

---
+++ RabbitMQClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQCommon;
+
+namespace RabbitMQClient
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            var counter = 0;
+
+            channel.QueueDeclare("hello", false, false, false, null);
+
+            while (true)
+            {
+                counter++;
+
+                var item = new LotItem
+                {
+                    Date = DateTime.UtcNow,
+                    Contract = counter,
+                    Lot = counter,
+                    Price = counter * 100m
+                };
+                var message = JsonConvert.SerializeObject(item);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "hello", null, body);
+
+                Console.WriteLine(message);
+            }
+
+        }
+    }
+}
---
+++ RabbitMQServer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQCommon;
+
+namespace RabbitMQServer
+{
+    internal class Program
+    {
+        private static readonly AutoResetEvent _waiter = new AutoResetEvent(false);
+
+        private static void Main()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello", false, false, false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += ConsumerOnReceived;
+
+            channel.BasicConsume("hello", true, consumer);
+
+            _waiter.WaitOne();
+        }
+
+        private static void ConsumerOnReceived(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            var item = JsonConvert.DeserializeObject<LotItem>(message);
+            var dateNow = DateTime.UtcNow;
+
+            Console.WriteLine(dateNow.Subtract(item.Date).ToString("g"));
+        }
+
+    }
+}
